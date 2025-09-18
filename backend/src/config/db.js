786f83b17d74fcbda4b2950f9304973fe1660c05
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-09-18 23:55:54 +0530

    done

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 
  export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://nayanaramanth2003_db_user:BEHsMpQtQv5tQqbj@cluster0.q5a0ldj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://nayanaramanth2003_db_user:BEHsMpQtQv5tQqbj@cluster0.q5a0ldj.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB connected successfully");
     }
     catch (error) {
         console.error("Error in DB connection", error);
+        process.exit(1);
     }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,15 @@ import express from 'express';
 //const express = require('express');
 import notesRoters from './routes/notesRotes.js';
 import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 const app = express();
+
+connectDB();
 
 app.use("/api/notes",notesRoters);
 
